7c556ca5f64c844df7048f569126836acdd38ef1
Anthony Campbell <anthony@cambo.io>
2018-06-21 18:08:18 +1000

    latest changes

--- index.js
+++ index.js
@@ -9,38 +9,40 @@ var config = require('./config.json');
 var strings = [];
 var latest = '000000000000';
 
+var latestPosted = {};
+
 var markov;
 var corpusBuilt = false;
 
 const compareIds = (x, y) => {
-   if(x.length > y.length){
+   if (x.length > y.length) {
       return true;
    }
 
-   if(x.length < y.length){
+   if (x.length < y.length) {
       return false;
    }
 
-   var xHead = x.slice(0,6);
-   var yHead = y.slice(0,6);
+   var xHead = x.slice(0, 6);
+   var yHead = y.slice(0, 6);
 
-   if(parseInt(xHead) > parseInt(yHead)){
+   if (parseInt(xHead) > parseInt(yHead)) {
       return true;
    }
 
-   if(parseInt(xHead) < parseInt(yHead)){
+   if (parseInt(xHead) < parseInt(yHead)) {
       return false;
    }
 
-   if(parseInt(xHead) == parseInt(yHead)){
-      var xMid = x.slice(6,6);
-      var yMid = y.slice(6,6);
+   if (parseInt(xHead) == parseInt(yHead)) {
+      var xMid = x.slice(6, 6);
+      var yMid = y.slice(6, 6);
 
-      if(parseInt(xMid) > parseInt(yMid)){
+      if (parseInt(xMid) > parseInt(yMid)) {
          return true;
       }
-   
-      if(parseInt(xMid) < parseInt(yMid)){
+
+      if (parseInt(xMid) < parseInt(yMid)) {
          return false;
       }
    }
@@ -48,9 +50,9 @@ const compareIds = (x, y) => {
    return false;
 }
 
-const readStrings = async () => {
+const readStrings = async() => {
    fs.readFile(__dirname + '/strings.json', (err, data) => {
-      if(err) {
+      if (err) {
          console.log(err);
          scrapeTimeline((data) => {
             console.log('scrape complete');
@@ -59,7 +61,7 @@ const readStrings = async () => {
          return;
       }
 
-      if(data){
+      if (data) {
          strings = JSON.parse(data);
          strings = cleanStrings(strings);
          writeStrings(strings);
@@ -73,12 +75,12 @@ const readStrings = async () => {
 const writeStrings = (strings) => {
    console.log('writing strings');
    fs.writeFile(__dirname + '/strings.json', JSON.stringify(strings), (err, data) => {
-      if(err) {
+      if (err) {
          console.log(err);
          return;
       }
 
-      if(data){
+      if (data) {
          console.log('strings written to file');
       }
    })
@@ -90,7 +92,9 @@ const cleanStrings = (strings) => {
       var reg = /^https?:\/\/.*$/;
       var split = string.split(" ");
       split = split.map(word => {
-         return reg.test(word) ? null : word;
+         return reg.test(word)
+            ? null
+            : word;
       });
       split = _.compact(split);
       var join = split.join(" ");
@@ -101,14 +105,14 @@ const cleanStrings = (strings) => {
    return tempStrings;
 }
 
-const readLatest = async () => {
+const readLatest = async() => {
    fs.readFile(__dirname + '/latest.json', (err, data) => {
-      if(err) {
+      if (err) {
          console.log(err);
          return;
       }
 
-      if(data){
+      if (data) {
          latest = JSON.parse(data);
          console.log('latest tweet loaded from file');
          return;
@@ -119,32 +123,34 @@ const readLatest = async () => {
 const writeLatest = (latest) => {
    console.log('writing latest', latest);
    fs.writeFile(__dirname + '/latest.json', JSON.stringify(latest), (err, data) => {
-      if(err) {
+      if (err) {
          console.log(err);
          return;
       }
 
-      if(data){
+      if (data) {
          latest = data;
          console.log('latest tweet written to file');
       }
    });
 }
 
-const scrapeTimeline = (callback) => {
+const scrapeTimeline = callback => {
    var readTo = latest;
 
    var timeline = new scrape.TimelineStream('brigidwd', {
       retweets: false,
       replies: false,
-      count: strings.length > 0 ? 50 : 5000
+      count: strings.length > 0
+         ? 50
+         : 5000
    });
 
    timeline.on('data', (chunk) => {
-      if(compareIds(chunk.id, readTo)){
+      if (compareIds(chunk.id, readTo)) {
          console.log('timeline data', chunk.id, chunk.text);
          strings.push(chunk.text);
-         if(compareIds(chunk.id, latest)){
+         if (compareIds(chunk.id, latest)) {
             latest = chunk.id;
          }
       }
@@ -167,27 +173,28 @@ const setupMarkov = () => {
    console.log('setting up markov');
    markov = new Markov(strings, opts);
    console.log('building corpus');
-   markov.buildCorpus()
-   .then(() => {
-      corpusBuilt = true;
-      console.log('corpus built');
-      return true;
-   });
+   markov
+      .buildCorpus()
+      .then(() => {
+         corpusBuilt = true;
+         console.log('corpus built');
+         return true;
+      });
 }
 
 const generateMarkov = (callback) => {
-   if(!corpusBuilt){ callback('no corpus to generate markov from'); 
-   setupMarkov();
-   return;}
+   if (!corpusBuilt) {
+      callback('no corpus to generate markov from');
+      setupMarkov();
+      return;
+   }
 
-   markov.generateSentence()
-   .then(result => callback(result));
+   markov
+      .generateSentence()
+      .then(result => callback(result));
 }
 
-var bot = new discord.Client({
-   token: config.token,
-   autorun: true
-});
+var bot = new discord.Client({token: config.token, autorun: true});
 
 bot.on('ready', async e => {
    console.log('logged in as', bot.username);
@@ -197,47 +204,40 @@ bot.on('ready', async e => {
 });
 
 bot.on('message', (user, uId, cId, message, e) => {
-   var reg = /^<@(\d{18})> !(\w*).*$/;
+   var reg = /^<(@|:delaney:)(\d{18})> ?!?(\w*)?.*$/;
    var del = /^<:delaney:\d{18}>$/;
 
-   if(del.test(message)){
-      generateMarkov(result => {
-         console.log(result);
-         bot.sendMessage({
-            to: cId,
-            message: result.string
-         });
-      });
+   var postMarkov = markov => {
+      console.log(markov);
+      latestPosted[cId] = markov;
+      bot.sendMessage({to: cId, message: markov.string});
+   };
+
+   if (del.test(message)) {
+      generateMarkov(result => postMarkov(result));
       return;
    }
-   
-   if(reg.test(message)){
-      var match = reg.exec(message);
-      if(match[1] == bot.id){
-         var cmd = match[2];
 
-         switch(cmd){
+   if (reg.test(message)) {
+      var match = reg.exec(message);
+      if (match[2] == bot.id || match[1] == ":delaney:") {
+         var cmd = match[3];
+
+         switch (cmd) {
             case 'scrape':
-               bot.sendMessage({
-                  to: cId,
-                  message: 'Scraping (the bottom of the barrel)'
-               });
+               bot.sendMessage({to: cId, message: 'Scraping (the bottom of the barrel)'});
                scrapeTimeline(() => {
                   setupMarkov();
-                  bot.sendMessage({
-                     to: cId,
-                     message: 'Scraping complete'
-                  });
+                  bot.sendMessage({to: cId, message: 'Scraping complete'});
                });
                break;
             case 'tweet':
-               generateMarkov(result => {
-                  console.log(result);
-                  bot.sendMessage({
-                     to: cId,
-                     message: result.string
-                  });
-               });
+               generateMarkov(result => postMarkov(result));
+               break;
+            case 'explain':
+            case 'why':
+               var explanation = latestPosted[cId] ? `\`\`\`js\n${JSON.stringify(latestPosted[cId], null, 3)}\n\`\`\`` : "Am I dumb or was nothing posted to this channel?";
+               bot.sendMessage({to: cId, message: explanation});
                break;
             default:
                console.log('some other request');
@@ -245,5 +245,3 @@ bot.on('message', (user, uId, cId, message, e) => {
       }
    }
 });
-
-scrapeTimeline(() => console.log('done'));
